ef22e64bfaab7a335b3ffab533f03051a5440789
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-07 18:47:33 -0400

    added some images and made a few changes to CSS, HTML and JS files

--- Assets/Files/battleship.js
+++ Assets/Files/battleship.js
@@ -1,1 +1,9 @@
+var view = {
+  function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
 
+  displayHit: function (location) {},
+  displayMiss: function (location) {},
+};
