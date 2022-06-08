eb4a8abc0df35938ef4d7e165cfd5ee5d2344f87
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-07 20:29:19 -0400

    added some code in JS file

--- Assets/Files/battleship.js
+++ Assets/Files/battleship.js
@@ -15,14 +15,34 @@ var view = {
   },
 };
 
-//set up "model" parameters for ships//
+//set up parameters for ships//
+var shipmodel = {
+  gameboardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+};
 
-//ships locations//
+//ships locations (array for coordinates), work on function to randomly generate ship locations//
+ships: [
+  {locations: [0,0,0], hits: ["", "", ""]},
+  {locations: [0,0,0], hits: ["", "", ""]},
+  {locations: [0,0,0], hits: ["", "", ""]},
 
-//fire function//
+],
 
-//generate ships function//
+//fire function, (index location sor ships--for-loop)//
+fire: function(guess) {
+  for (var i = 0; i < this.numShips; i++)
+}
 
-//guess functions//
+//isSunk function (use index location for ships and boolean to determine if ship is sunk)
 
-//event handlers for button click and keypress//
+
+
+//function for guesses, (add criteria that =null if guess outside of coordinate area)//
+  //use parseGuess maybe?? Also, need to research how to convert column letters to numbers//
+
+
+  
+//event handlers for fire button click and keypress for entering guess coordinates//
