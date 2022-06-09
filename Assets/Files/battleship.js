064d86d986d351ac5c0dbb01606063c4b9daebef
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-09 19:06:33 -0400

    added ship functions in JS file

--- Assets/Files/battleship.js
+++ Assets/Files/battleship.js
@@ -23,29 +23,76 @@ var shipmodel = {
   shipsSunk: 0,
 };
 
-//ships locations (arrays for coordinates and hits)//
-var ships = [
+ships = [
   {locations: [0,0,0], hits: ["", "", ""]},
   {locations: [0,0,0], hits: ["", "", ""]},
-  {locations: [0,0,0], hits: ["", "", ""]},
-];
+  {locations: [0,0,0], hits: ["", "", ""]}, ],
 
-// function to randomly generate ship locations (Math.random or Math.floor or both maybe?--need to research this more)//
-  //also need to generate a starting column/row and new ship location//
-generateShip = function() {
-
+fire: function(guess) {
+  for (var i = 0; i < this.numShips; i++) {
+    var ship = this.ships[i];
+    var index = ship.locations.indexOf(guess);
+    if (index >= 0) {
+      ship.hits[index] = "hit";
+      view.displayMessage("HIT!");
+      if (this.isSunk(ship)) {
+        view.displayMessage("You Sank My Battleship!");
+        this.shipsSunk++;
+      }
+      return true;
+   }
 }
+    
+      view.displayMiss(guess);
+      view.displayMessage("You Missed!");
+      return false;
+},
+
+isSunk = function(ship) {
+  for (var i = 0; i < this.shipLength; i++) {
+    if (ship.hits[i] !== "hit") {
+      return false;
+    }
+  }
+  return true;
+};
 
 
-//fire function, (index location sor ships--for-loop)//
-fire = function(guess) {
-  for (var i = 0; i < this.numShips; i++)
-}
+  generateShipLocations = function () {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+    
+  },
 
-//Sunk function (use index location for ships and boolean to determine if ship is sunk?)
-isSunk = function(ship){
+  generateShip = function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row;
+    var col;
+    if (direction === 1) {
+      var newShipLocations = [];
+      for (var i = 0; i < this.shipLength; i++);{
+        if (direction === 1) {
+        } else {
+        }
+      } 
+    return newShipLocations;
+  },
 
-}
+
+
+
+
+
+
+
+
+
+
 
 
 //function for guesses, (add criteria that =null if guess outside of coordinate area)//
@@ -56,9 +103,17 @@ isSunk = function(ship){
 
 
 //event handlers for fire button click and keyboard keypress for entering guess coordinates in input box//
-var fireButton = document.getElementById("fireButton");
-fireButton.onclick = handleFireButton;
+function init () {
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+}
 
-function handleFireButton() {
-
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
 }
