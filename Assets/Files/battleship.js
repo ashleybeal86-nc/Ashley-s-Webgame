ca880c0b11067168ed3935cdbf3b0aa61a916c8b
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-12 11:10:24 -0400

    fixed a few issues in my code

--- Assets/Files/battleship.js
+++ Assets/Files/battleship.js
@@ -1,4 +1,4 @@
-var shipmodel = {
+var model = {
   boardSize: 7,
   numShips: 3,
   shipLength: 3,
@@ -116,8 +116,8 @@ var controller = {
     var location = parseGuess(guess);
     if (location) {
       this.guesses++;
-      var hit = shipmodel.fire(location);
-      if (hit && shipmodel.shipsSunk === shipmodel.numShips) {
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
         view.displayMessage(
           "You sank all my battleships, in " + this.guesses + " guesses"
         );
@@ -130,19 +130,19 @@ function parseGuess(guess) {
   var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
   if (guess === null || guess.length !== 2) {
-    alert("Oops, please enter a letter and a number on the board.");
+    alert("Oops, please enter a letter and a number on the board!");
   } else {
     var firstChar = guess.charAt(0);
     var row = alphabet.indexOf(firstChar);
     var column = guess.charAt(1);
 
     if (isNaN(row) || isNaN(column)) {
-      alert("Oops, that isn't on the board.");
+      alert("Oops, that isn't on the board!");
     } else if (
       row < 0 ||
-      row >= shipmodel.boardSize ||
+      row >= model.boardSize ||
       column < 0 ||
-      column >= shipmodel.boardSize
+      column >= model.boardSize
     ) {
       alert("Oops, that's off the board!");
     } else {
